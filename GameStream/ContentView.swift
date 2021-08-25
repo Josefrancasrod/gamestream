7775af718e10075e89944902d0d6aab6b7340e2b
Jose Francisco Castillo Rodriguez <josefrancasrod@gmail.com>
2021-08-24 23:54:05 -0500

    First Commit

---
+++ GameStream/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  GameStream
+//
+//  Created by José Francisco Castillo Rodríguez on 24/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Image("04_swiftui-apps-ios-pantalla1").resizable()
+        ContentView()
+    }
+}
