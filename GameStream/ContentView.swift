1f0268791325c77fbc8bf3f53ef61fcc6f8f45a1
Jose Francisco Castillo Rodriguez <josefrancasrod@gmail.com>
2021-08-25 23:03:44 -0500

    -Login Screen - Add logic to the login and register buttons

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            
+            Spacer()
             //Color(.blue).ignoresSafeArea()
             Color( red: 19/255, green: 30/255, blue: 60/255,opacity: 1.0  ).ignoresSafeArea()
             VStack{
-                Image("04_swiftui-apps-ios-logo-name").resizable().aspectRatio(contentMode: .fit).frame(width: 250)
+                Image("04_swiftui-apps-ios-logo-name").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 42)
                 InicioYRegistroView()
             }
             
@@ -23,12 +23,46 @@ struct ContentView: View {
     }
 }
 
+struct InicioSesionView: View {
+    var body: some View{
+        VStack{
+            Text("inicio")
+        }
+    }
+}
+
+struct RegistroView: View {
+    var body: some View{
+        VStack{
+            Text("Registro")
+        }
+    }
+}
 struct InicioYRegistroView:View {
+    @State var tipoInicioSesion = true
+    
     var body: some View{
         VStack{
             HStack {
-                Text("INICIA SESION")
-                Text("REGISTRATE")
+                Spacer()
+                Button("INICIA SESIÓN"){
+                    tipoInicioSesion = true
+                    print("Inicio de sesión")
+                }.foregroundColor(tipoInicioSesion ? .white : .gray)
+                Spacer()
+                Button("REGISTRATE"){
+                    tipoInicioSesion = false
+                    print("Registrando...")
+                }.foregroundColor(tipoInicioSesion ? .gray : .white)
+                Spacer()
+                
+            }
+            
+            Spacer(minLength: 42)
+            if tipoInicioSesion == true {
+                InicioSesionView()
+            }else{
+                RegistroView()
             }
         }
     }
