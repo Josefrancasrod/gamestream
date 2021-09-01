3e2e437c560d34193d8174621410b9a6a04bb5a0
Jose Francisco Castillo Rodriguez <josefrancasrod@gmail.com>
2021-08-31 23:17:57 -0500

    Home Screen - Add background color and style to tabBar

--- GameStream/Home.swift
+++ GameStream/Home.swift
@@ -8,29 +8,48 @@
 import SwiftUI
 
 struct Home: View {
+    @State var tabSeleccionado: Int = 2
     var body: some View {
-        TabView {
-            Text("Pantalla Home").font(.system(size: 30, weight: .bold, design: .rounded)).tabItem {
-                Image(systemName: "house")
-                Text("Home")
-            }
+        TabView (selection: $tabSeleccionado){
             
             Text("Pantalla Perfil").font(.system(size: 30, weight: .bold, design: .rounded)).tabItem {
                 Image(systemName: "person")
                 Text("Perfil")
-            }
+            }.tag(0)
             
             Text("Pantalla Juegos").font(.system(size: 30, weight: .bold, design: .rounded)).tabItem {
                 Image(systemName: "gamecontroller")
                 Text("Juegos")
-            }
+            }.tag(1)
+            
+           PantallaHome().tabItem {
+                Image(systemName: "house")
+                Text("Home")
+            }.tag(2)
             
             Text("Pantalla Favoritos").font(.system(size: 30, weight: .bold, design: .rounded)).tabItem {
                 Image(systemName: "star")
                 Text("Favoritos")
-            }
-        }
+            }.tag(3)
+        }.accentColor(.white)
+    }
+    
+    init(){
+        UITabBar.appearance().barTintColor = UIColor(Color("TabBarColor"))
+        UITabBar.appearance().isTranslucent = true
         
+        print("Iniciando las vistas de home")
+    }
+}
+
+struct PantallaHome: View{
+    var body: some View{
+        ZStack{
+            Color("Marine").ignoresSafeArea()
+            VStack{
+                
+            }.padding(.horizontal, 10)
+        }.navigationBarHidden(true).navigationBarBackButtonHidden(true)
     }
 }
 
